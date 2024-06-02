6b89768e8caffe449b326e458687d02569ed1041
Franco Orozco <franco.nahuel.orozco@gmail.com>
2024-06-02 01:07:05 -0500

    realice varios cambios al proyecto

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -10,10 +10,10 @@ mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    return render_template ("index.html")
+    return render_template("index.html")
 
-@app.route("/add_contact", methods=["POST"])
-def add_contact():
+@app.route("/pedidos", methods=["POST"])
+def pedidos():
     if request.method == "POST":
         
         id_cliente = request.form["id_cliente"]
@@ -24,7 +24,14 @@ def add_contact():
         cur = mysql.connection.cursor()
         cur.execute('INSERT INTO pedido (id_cliente, nombre, direccion, tel, tipo_pizza) VALUES (%s,%s,%s,%s,%s)',(id_cliente, fullname, direccion, phone, tipo_pizza))
         mysql.connection.commit()
-        return "SEND"
+        return render_template("pedido.html")
+    
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+
 
 if __name__=="__main__":
     app.run(port=5021,debug=True)
